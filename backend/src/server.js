2a9dc6eb129553a1f911de020b1cced480e1d0fa
Tushar-Shinde45 <149402930+Tushar-Shinde45@users.noreply.github.com>
2025-08-19 12:14:54 +0530

    chore: initialize backend project with package.json and essential dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import { connectDB } from "./config/db.js";
 import complaintRoutes from "./routes/complaintRoutes.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import authRoutes from "./routes/authRoutes.js";
+import path from "path";
 
 // Load env vars
 dotenv.config();
@@ -14,6 +15,8 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+const _dirname = path.resolve();
 
 // Middleware
 app.use(cors());
@@ -30,6 +33,11 @@ app.use("/api/auth", authRoutes);
 
 // Error handler
 app.use(errorHandler);
+
+app.use(express.static(path.join(_dirname, "frontend/dist")));
+app.get("/*splat", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
